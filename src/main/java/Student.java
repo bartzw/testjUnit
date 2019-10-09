ad456df9aed40ced76716750efb805eb7f2cc4e0
Bart Zwaagstra <bartzwaagstra@live.nl>
2019-10-09 07:54:23 +0200

    update

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -7,63 +7,53 @@ import java.io.*;
 // A class to represent a student. 
 class Student
 {
-    int rollno;
+    int studentnumber;
     String name, address;
 
     // Constructor 
-    public Student(int rollno, String name,
-                   String address)
-    {
-        this.rollno = rollno;
+    public Student(int studentnumber, String name, String address) {
+        this.studentnumber = studentnumber;
         this.name = name;
         this.address = address;
     }
 
     // Used to print student details in main() 
-    public String toString()
-    {
-        return this.rollno + " " + this.name +
-                " " + this.address;
+    public String toString() {
+        return this.studentnumber + " " + this.name + " " + this.address;
     }
 }
 
-class Sortbyroll implements Comparator<Student>
-{
+class Sortbystudentnumber implements Comparator<Student> {
     // Used for sorting in ascending order of 
     // roll number 
-    public int compare(Student a, Student b)
-    {
-        return a.rollno - b.rollno;
+    public int compare(Student a, Student b) {
+        return a.studentnumber - b.studentnumber;
     }
 }
 
-class Sortbyname implements Comparator<Student>
-{
+class Sortbyname implements Comparator<Student> {
     // Used for sorting in ascending order of 
     // roll name 
-    public int compare(Student a, Student b)
-    {
+    public int compare(Student a, Student b) {
         return a.name.compareTo(b.name);
     }
 }
 
 // Driver class 
-class Main
-{
-    public static void main (String[] args)
-    {
+class Main {
+    public static void main (String[] args) {
         ArrayList<Student> ar = new ArrayList<Student>();
-        ar.add(new Student(111, "bbbb", "london"));
-        ar.add(new Student(131, "aaaa", "nyc"));
-        ar.add(new Student(121, "cccc", "jaipur"));
+        ar.add(new Student(100, "Ben", "london"));
+        ar.add(new Student(105, "Bart", "nyc"));
+        ar.add(new Student(110, "Lisa", "jaipur"));
 
         System.out.println("Unsorted");
         for (int i=0; i<ar.size(); i++)
             System.out.println(ar.get(i));
 
-        Collections.sort(ar, new Sortbyroll());
+        Collections.sort(ar, new Sortbystudentnumber());
 
-        System.out.println("\nSorted by rollno");
+        System.out.println("\nSorted by studentnumber");
         for (int i=0; i<ar.size(); i++)
             System.out.println(ar.get(i));
 
